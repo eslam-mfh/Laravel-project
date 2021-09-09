a16f6cf6d3f564c4f99b83cbfd930653ec5a1f10
Eslam <eslam.mfh@gmail.com>
2021-09-09 10:56:01 +0300

    إضافة controllers جديدة وتعديل الملفات القديمة

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\OfferController;
 use App\Http\Controllers\OfferServiceController;
 use App\Http\Controllers\ReviewsController;
 use App\Http\Controllers\ServiceController;
-use App\Http\Controllers\ServiceSpecialistController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\SpecialistController;
 use Illuminate\Support\Facades\Route;
@@ -35,8 +34,8 @@ Route::get('/specialist/{id}',[specialistController::class , 'show']); // show s
 ///
 Route::get('categories' , [CategoryController::class , 'index']); //show specialists
 Route::get('/category/{id}',[CategoryController::class , 'show']); // show specialist information
-Route::get('addCategory',[CategoryController::class , 'store']); // show specialist information
-Route::get('/editCategory/(id}',[CategoryController::class , 'update']); // show specialist information
+Route::post('addCategory',[CategoryController::class , 'store']); // show specialist information
+Route::post('/editCategory/{id}',[CategoryController::class , 'update']); // show specialist information
 Route::get('/deleteCategory/{id}',[CategoryController::class , 'destroy']); // show specialist information
 
 //
@@ -46,7 +45,7 @@ Route::get('topServices' , [ServiceController::class , 'index']); //show special
 Route::get('deleteService/{id}' , [ServiceController::class , 'destroy']); //show specialists
 Route::post('addService' , [ServiceController::class , 'store']); //show specialists
 Route::post('editService/{id}' , [ServiceController::class , 'update']); //show specialists
-Route::get('Services/{id}' , [serviceController::class, "getCategoryDashboard"]) ; //services names for specific category
+Route::get('Services/{id}' , [serviceController::class, 'getCategoryDashboard']) ; //services names for specific category
 
 
 ////
@@ -59,18 +58,17 @@ Route::get('deleteOffer/{id}'  , [OfferController::class , 'destroyTypeZero']); 
 Route::get('deleteSoonOffer/{id}'  , [OfferController::class , 'destroyTypeOne']); //show specialists
 Route::get('offerDetails/{id}' , [OfferController::class , 'services']); //show specialists
 Route::get('/offer-service/link', [OfferServiceController::class, 'store']);
-
 ///////////
 Route::get('allmedia', [MediaController::class, 'index']); //show before & after
-Route::get('upload', [MediaController::class, 'upload']);//upload before & after
-Route::get('/editMedia/{id}', [MediaController::class, 'update']);
+Route::post('upload', [MediaController::class, 'upload']);//upload before & after
+Route::post('/editMedia/{id}', [MediaController::class, 'update']);
 Route::get('/deleteMedia/{id}', [MediaController::class, 'destroy']);
 Route::get('media/{id}', [MediaController::class, 'show']); //show before & after
 ///////////////////////
 Route::get('/advice', [AdviceController::class, 'index']);
 Route::post('/advice', [AdviceController::class, 'store']);
 Route::get('/advice/{id}', [AdviceController::class, 'show']);
-Route::put('/advice/{id}', [AdviceController::class, 'update']);
+Route::put('/editAdvice/{id}', [AdviceController::class, 'update']);
 Route::delete('/advice/{id}', [AdviceController::class, 'destroy']);
 /////////////////
 Route::get('/approveSession/{id}', [SessionController::class, 'approveSession']);
@@ -85,36 +83,13 @@ Route::get('/reviews', [ReviewsController::class, 'index']);
 Route::get('/users', [AuthController::class, 'allUsers']);
 Route::get('/userDetails/{id}', [AuthController::class, 'userDetails']);
 Route::get('/pendingReviews', [ReviewsController::class, 'getPendingReviews']);
-Route::get('approveReview', [ReviewsController::class, 'approveReview']);
-//Route::get('addSpecialist', [specialistController::class , 'store']);//edit specialist
+Route::get('approveReview/{reviewId}', [ReviewsController::class, 'approveReview']);
+//////////////////////
+Route::post('addSpecialist', [specialistController::class , 'store']);//edit specialist
+Route::get('/deleteSpecialist/{id}',[specialistController::class,'destroy']);// delete specialist
+Route::post('/editSpecialist/{id}', [specialistController::class , 'update']);//edit specialist
 
 Route::get('/link-service-specialist/{service_id}/{specialist_id}', [ServiceSpecialistController::class, 'linkServiceToSpecialist']);
 Route::get('/unlink-service-specialist/{service_id}/{specialist_id}', [ServiceSpecialistController::class, 'unlinkServiceFromSpecialist']);
-
-
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('addSpecialist', [specialistController::class , 'store']);//edit specialist
-    Route::get('/deleteSpecialist/{id}',[specialistController::class,'destroy']);// delete specialist
-    Route::get('/editSpecialist/{id}', [specialistController::class , 'update']);//edit specialist
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
